5dff29d3099827596ec62716a26ff3dc3f854e09
Josuex09 <josuex09@hotmail.es>
2015-05-18 18:19:33 -0600

    services
    Services v1

---
+++ src/main/java/services/SessionService.java
@@ -0,0 +1,9 @@
+package Services;
+
+public interface SessionService {
+	public void signUp(String username,String password);
+	public void logIn(String username,String password);
+	public void logOut(String username);
+	public boolean validateEmail(String email);
+
+}
