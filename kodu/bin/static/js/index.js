3ae7304f38dbee414b4216064cd3849c0e8273c5
Jason <jasoncc95@gmail.com>
2015-06-14 00:30:15 -0600

    login and nav bar edition

--- kodu/bin/static/js/index.js
+++ kodu/bin/static/js/index.js
@@ -1,23 +1,21 @@
 $(document).ready(function(){
     $("#datos-registro").hide();
-    $("#registrarse").css("background-color","#c0c0c0");
+    $("#registrarse").css("background-color","#E9E9E9");
     $("#iniciar").css("background-color","#fff");
     
     
     $("#iniciar").click(function(){
         $("#iniciar").css("background-color","#fff");
-        $("#registrarse").css("background-color","#c0c0c0");
+        $("#registrarse").css("background-color","#E9E9E9");
         $("#datos-iniciar").show();
         $("#datos-registro").hide();
     });
     
     $("#registrarse").click(function(){
         $(this).css("background-color","#fff");
-        $("#iniciar").css("background-color","#c0c0c0");
+        $("#iniciar").css("background-color","#E9E9E9");
         $("#datos-iniciar").hide();
         $("#datos-registro").show();
     });
-    
-    
-    
+  
 })
